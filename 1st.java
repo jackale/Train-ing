042fb8ba1442f461b4d048bcfa4cd1bbe2bd48bc
jackale <melon119@gmail.com>
2018-04-16 15:38:55 +0900

    Update
    tsurai

--- 1st.java
+++ 1st.java
@@ -1,8 +1,12 @@
+enum ShapeType = {circle, square};
+
 interface Shape {
+    const static ShapeType SHAPE_TYPE;
     void Draw();
 }
 
 class Circle implements Shape {
+    const static ShapeType SHAPE_TYPE = ShapeType.circle;
     public Circle(int radius, Point center) {
         this.radius = radius;
         this.center = center;
@@ -10,9 +14,11 @@ class Circle implements Shape {
     public Draw() {
         /* 描く処理 */
     }
+    public ShapeType getType() { return SHAPE_TYPE; }
 }
 
 class Square implements Shape {
+    const static ShapeType SHAPE_TYPE = ShapeType.square;
     public Square(int side, Point topLeft) {
         this.side    = side;
         this.topLeft = topLeft;
@@ -20,12 +26,16 @@ class Square implements Shape {
     public Draw() {
         /* 描く処理 */
     }
+    public ShapeType getType() { return SHAPE_TYPE; }    
 }
 
 class DrawingTool {
     public DrawAllShapes(List<Shape> shapeList) {
         for (Shape shape : shapeList) {
-            shape.Draw();
+            if (shape.getType() == ShapeType.circle) shape.Draw();
+        }
+        for (Shape shape : shapeList) {
+            if (shape.getType() == ShapeType.square) shape.Draw();
         }
     }
 }
